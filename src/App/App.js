34166423e47b3d5966f9c2c6b2fe1ca9c34bfa26
Carissa Gross <carissamgross@gmail.com>
2022-10-31 09:48:25 -0600

    Update API calls to get reservation information

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import Reservations from '../Reservations/Reservations'
+import { getReservations } from '../apiCalls.js'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      reservations: []
+      reservations: [],
+      error: ''
     }
   }
+
+  componentDidMount = async() => {
+    try {
+      const result = await getReservations()
+
+      if(!result.ok) {
+        console.log(result.status)
+        throw new Error('Error!')
+      }
+
+      const data = await result.json()
+      this.setState({reservations: data})
+    } catch (error) {
+      this.setState({
+        error: "There was an error fetching your reservations"
+      })
+    }
+  }
+
+
   render() {
+    console.log(this.state.reservations)
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
