28a73dfb178df6bd4c3e0b4fd3afdf0212358101
Carissa Gross <carissamgross@gmail.com>
2022-10-31 09:29:02 -0600

    Update and create component file structure and additional organization

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reservations: []
+    }
+  }
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
         </div>
         <div className='resy-container'>
           
