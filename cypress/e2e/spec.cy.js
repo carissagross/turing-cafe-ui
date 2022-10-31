a83bd28f5fccd488039137f6778cc32fb2105600
Carissa Gross <carissamgross@gmail.com>
2022-10-31 10:44:30 -0600

    Create cypress testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Reservations', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
   })
 })
